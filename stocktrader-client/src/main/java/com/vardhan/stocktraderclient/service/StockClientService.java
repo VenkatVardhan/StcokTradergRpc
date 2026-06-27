618593aa1d6ea3487401feebd6793646c29a8956
Venkat-Vardhan <saivenkat1579@gmail.com>
2026-06-27 22:50:15 +0530

    updated stockclient

--- stocktrader-client/src/main/java/com/vardhan/stocktraderclient/service/StockClientService.java
+++ stocktrader-client/src/main/java/com/vardhan/stocktraderclient/service/StockClientService.java
@@ -10,8 +10,9 @@ import org.springframework.stereotype.Service;
 public class StockClientService {
     @GrpcClient("stockService") //any name
     private StockTradingServiceGrpc.StockTradingServiceBlockingStub serviceBlockingStub;
+    //blocking stub is unary communication similar to native rest
 
-            //grpc server has StockResponse method(StockRequest) so call that
+            //grpc server has StockResponse method(StockRequest) so call that in the code using blocking stub
     public StockResponse getStockPrice(String stockSymbol){
         StockRequest request = StockRequest.newBuilder().setStockSymbol(stockSymbol).build();
         StockResponse response = serviceBlockingStub.getStockPrice(request);
